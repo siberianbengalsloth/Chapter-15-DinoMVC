17a8c0d2e2629fb177de28677da0fb124388fd56
dovmfdyd3@gmail.com <tp9m96Nd@fXty8xhPhu3!>
2023-02-10 01:41:18 +0900

    first commit

---
+++ src/main/java/org/launchcode/Chapter5/Lecture/Chapter15LectureApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Chapter5.Lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter15LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter15LectureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/launchcode/Chapter5/Lecture/data/DinoData.java
@@ -0,0 +1,4 @@
+package org.launchcode.Chapter5.Lecture.data;
+
+public class DinoData {
+}
